bbd6d4cbd6ffe8564184112cb5ddfc50eed7909e
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2022-02-17 18:09:17 +0700

    Add files via upload

---
+++ Mark.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+public class Mark {
+	private String sname;
+	private int tamil;
+	private int english;
+	private int maths;
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public int getTamil() {
+		return tamil;
+	}
+	public void setTamil(int tamil) {
+		this.tamil = tamil;
+	}
+	public int getEnglish() {
+		return english;
+	}
+	public void setEnglish(int english) {
+		this.english = english;
+	}
+	public int getMaths() {
+		return maths;
+	}
+	public void setMaths(int maths) {
+		this.maths = maths;
+	}
+	
+	
+
+}
