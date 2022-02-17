23611f2c99954870e52f65e66b06b4c71ee5cab8
kalaivanijava <93625785+kalaivanijava@users.noreply.github.com>
2022-02-17 18:10:54 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class Book {
+	private String bName;
+	private String bID;
+	private String bAuthor;
+	public String getbName() {
+		return bName;
+	}
+	public void setbName(String bName) {
+		this.bName = bName;
+	}
+	public String getbID() {
+		return bID;
+	}
+	public void setbID(String bID) {
+		this.bID = bID;
+	}
+	public String getbAuthor() {
+		return bAuthor;
+	}
+	public void setbAuthor(String bAuthor) {
+		this.bAuthor = bAuthor;
+	}
+	
+
+}
